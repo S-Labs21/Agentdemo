b247bb2a1e2fdf447c8f419233515f452793379d
labsN0I12 <shaktilabhaniya@gmail.com>
2025-04-19 19:48:43 +0530

    updated codes

---
+++ agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+import gemma as gm
+from google.adk.models import Gemini
+import os
+from litellm import completion
+
+os.environ["GEMINI_API_KEY"] = "AIzaSyAc6824VbYbWH31UJtm0_gifeTv2uhVleU"
+
+root_agent = Agent(
+    name = "simple_chat",
+    model=LiteLlm(model="gemini-1.5-flash"),
+
+    description = "A simple conversational AI",
+    instruction = "Your name is reno, you are a conversational AI, you will act as a chat buddy"
+)
